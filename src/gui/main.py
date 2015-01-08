2970dc310f254aeea0627a8ad7ffb76c5b7ca3fb
Adrien-Dechelette <adrien.dechelette@gmail.com>
2015-01-08 12:30:05 +0100

    GUI Data preload
    Data load

--- src/gui/main.py
+++ src/gui/main.py
@@ -50,7 +50,7 @@ class Gui(QtGui.QWidget, GUI.Ui_GUI):
 		print('Socket Created')
 
 		try:
-                        self.s.connect((ip, port))
+			self.s.connect((ip, port))
 
 		except socket.error:
 			print('Failed to connect socket')
@@ -66,9 +66,9 @@ class Gui(QtGui.QWidget, GUI.Ui_GUI):
 		self.s.Close()
 
 	def play(self):
-                print("starting")
+		print("starting")
 		self.s.send("start")
-                thread.start_new_thread(self.run, ())
+		thread.start_new_thread(self.run, ())
 
 	def pause(self):
 		self.s.send('stop')
@@ -76,7 +76,7 @@ class Gui(QtGui.QWidget, GUI.Ui_GUI):
 	def cancel(self):
 		"""End this timer thread"""
 		self.cancelled = True
-                
+
 	def init_Ui(self, ip, port):
 		# currentCellChanged ( int currentRow, int currentColumn, int previousRow, int previousColumn )
 		# self.tableWidget.currentItemChanged.connect(self.affPacket)
@@ -236,7 +236,9 @@ class Gui(QtGui.QWidget, GUI.Ui_GUI):
 			self.loadUDP(packet)
 
 		document = QtGui.QTextDocument(self.plainTextEditData)
-		document.setPlainText(binascii.unhexlify(document.toPlainText()))
+
+		document.setPlainText(binascii.hexlify(packet[1]['Data']))#binascii.unhexlify() 195.154.71.44
+
 		documentLayout = QtGui.QPlainTextDocumentLayout(document)
 		document.setDocumentLayout(documentLayout)
 		self.plainTextEditData.setDocument(document)
@@ -246,18 +248,18 @@ class Gui(QtGui.QWidget, GUI.Ui_GUI):
 	def sendPacket(self):
 		packet = [{}, {}]
 
-                p = Ethernet()
+		p = Ethernet()
 		packet[0]['Mac Destination'] = self.lineEditMacDestination.text()
 		packet[0]['Mac Source'] = self.lineEditMacSource.text()
 
-                p.dst = binascii.unhexlify("".join(packet[0]['Mac Destination'].split(':')))
-                p.src = binascii.unhexlify("".join(packet[0]['Mac Source'].split(":")))
-                p.data = IP()
-                p.type = 0x0800
+		p.dst = binascii.unhexlify("".join(packet[0]['Mac Destination'].split(':')))
+		p.src = binascii.unhexlify("".join(packet[0]['Mac Source'].split(":")))
+		p.data = IP()
+		p.type = 0x0800
 
-                print(p)
+		print(p)
 
-                packet[0]['Time'] = self.lineEditTime.text()
+		packet[0]['Time'] = self.lineEditTime.text()
 		packet[0]['Version'] = int(self.lineEditVersion.text())
 		packet[0]['Header Length'] = int(self.lineEditHeaderLength.text())
 		packet[0]['TTL'] = int(self.lineEditTTL.text())
@@ -265,38 +267,36 @@ class Gui(QtGui.QWidget, GUI.Ui_GUI):
 		packet[0]['IP Source'] = self.lineEditIPSource.text()
 		packet[0]['IP Destination'] = self.lineEditIPDestination.text()
 
-                p.data.ttl = packet[0]['TTL']
+		p.data.ttl = packet[0]['TTL']
  
-                p.data.src=socket.inet_aton(packet[0]['IP Source'])
-                p.data.dst=socket.inet_aton(packet[0]['IP Destination'] )
+		p.data.src=socket.inet_aton(packet[0]['IP Source'])
+		p.data.dst=socket.inet_aton(packet[0]['IP Destination'] )
 
-                
-                if self.groupBoxTCPPart.isEnabled():
-                        payload = TCP()
-                        packet[1]['Source Port'] = int(self.lineEditSourcePort.text())
-                        payload.seq = int(self.lineEditSequence.text())
-                        packet[1]['Destination Port'] = int(self.lineEditDestinationPort.text())
-                        packet[1]['Sequence'] = self.lineEditSequence.text()
-                        packet[1]['Reconnaissance'] = self.lineEditReconnaissance.text()
-                        packet[1]['Header TCP'] = self.lineEditHeaderTCP.text()
-                elif self.groupBoxUDPPart.isEnabled():
-                        payload = UDP()
-                        packet[1]['Source Port'] = int(self.lineEditSourcePortUDP.text())
-                        packet[1]['Destination Port'] = int(self.lineEditDestinationPortUDP.text())
-                        packet[1]['Header Length'] = int(self.lineEditSizeUDP.text())
-                        packet[1]['Checksum'] = hex(self.lineEditChecksumUDP.text())
-                document = self.plainTextEditData.document()
-                payload.data = document.toPlainText()
-                payload.dport = packet[1]['Source Port']
-                payload.sport = packet[1]['Destination Port']
-                        
-                        
-#                except:
+		if self.groupBoxTCPPart.isEnabled():
+			payload = TCP()
+			packet[1]['Source Port'] = int(self.lineEditSourcePort.text())
+			payload.seq = int(self.lineEditSequence.text())
+			packet[1]['Destination Port'] = int(self.lineEditDestinationPort.text())
+			packet[1]['Sequence'] = self.lineEditSequence.text()
+			packet[1]['Reconnaissance'] = self.lineEditReconnaissance.text()
+			packet[1]['Header TCP'] = self.lineEditHeaderTCP.text()
+		elif self.groupBoxUDPPart.isEnabled():
+			payload = UDP()
+			packet[1]['Source Port'] = int(self.lineEditSourcePortUDP.text())
+			packet[1]['Destination Port'] = int(self.lineEditDestinationPortUDP.text())
+			packet[1]['Header Length'] = int(self.lineEditSizeUDP.text())
+			packet[1]['Checksum'] = hex(self.lineEditChecksumUDP.text())
+		document = self.plainTextEditData.document()
+		payload.data = binascii.unhexlify(document.toPlainText())
+		payload.dport = packet[1]['Source Port']
+		payload.sport = packet[1]['Destination Port']
+
+
+# 		except:
 #                        print("error")
 #                        pass
-                self.s.send("INJECT"+str(p))
-
-                return
+		self.s.send("INJECT"+str(p))
+		return
 
 
 
@@ -343,10 +343,10 @@ class Gui(QtGui.QWidget, GUI.Ui_GUI):
 		data_size = len(packet) - h_size
 
 		data = packet[h_size:]
-		ICMPdict['DATA'] = data
+		ICMPdict['Data'] = data
 		l.append(ICMPdict)
 		return
-                
+
 	def parse_UDP(self, packet, iph_length, eth_length, l):
 		u = iph_length + eth_length
 		udph_length = 8
@@ -366,7 +366,7 @@ class Gui(QtGui.QWidget, GUI.Ui_GUI):
 		UDPdict = {'Source Port':str(source_port), 'Destination Port':str(dest_port), 'Header Length':str(length), 'Checksum':str(checksum), "Size":str(data_size)}      
 
 		data = packet[h_size:]
-		UDPdict['data'] = data
+		UDPdict['Data'] = data
 		l.append(UDPdict)
 		return
 
@@ -522,7 +522,7 @@ def main(argv):
 	return app.exec_()
 
 if __name__=='__main__':
-    if len(sys.argv) >= 1:
-        main(sys.argv)
-    else:
-        print('Usage: python GUI')
+	if len(sys.argv) >= 1:
+		main(sys.argv)
+	else:
+		print('Usage: python GUI')
